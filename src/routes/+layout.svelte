47b278987bb4bbcf324a502cb99f4a06d721c8a4
DeKok, Matt G <MGD3@na.valmont.com>
2023-07-02 10:53:25 -0500

    Fading Loader Page Transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
 	import { navigating } from "$app/stores";
 	import { pageLoader } from "$src/lib/store";
+	import { fade } from "svelte/transition";
 	import "../app.css";
-
-	let loading = false;
-	let timeout: number;
-	$: if ($navigating) {
-		timeout = setTimeout(() => {
-			loading = true;
-		}, 200);
-	} else {
-		loading = false;
-		clearTimeout(timeout);
-	}
 </script>
 
 <svelte:body class="min-h-screen text-base-content" />
@@ -25,9 +15,9 @@
 
 <slot />
 
-{#if $pageLoader || loading}
-	<div class="fixed inset-0 z-40 flex items-center justify-center bg-black/50" />
-	<div class="fixed inset-0 z-50 flex items-center justify-center">
+{#if $pageLoader || $navigating}
+	<div class="fixed inset-0 z-40 flex items-center justify-center bg-black/50" transition:fade />
+	<div class="fixed inset-0 z-50 flex items-center justify-center" transition:fade>
 		<span class="loading loading-spinner w-16 text-secondary" />
 	</div>
 {/if}
