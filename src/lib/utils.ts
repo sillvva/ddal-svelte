19e22af6b3fa0ade74744e75df23d8f80341c247
DeKok, Matt G <MGD3@na.valmont.com>
2023-07-04 13:37:03 -0500

    Remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -37,8 +37,6 @@ export const tooltipClasses = (text?: string | null, align = "center") => {
 	);
 };
 
-export const canUseDOM = () => !!(typeof window !== "undefined" && window.document && window.document.createElement);
-
 export const sorter = (a: string | number | Date, b: string | number | Date) => {
 	if (typeof a === "string" && typeof b === "string") return a.localeCompare(b);
 	if (typeof a === "number" && typeof b === "number") return a - b;
