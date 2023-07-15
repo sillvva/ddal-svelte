60c18d62d02e018035bcfb7f8b24d1d1a22c2c05
DeKok, Matt G <MGD3@na.valmont.com>
2023-07-15 10:20:39 -0500

    Tweak modal text size

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -119,7 +119,7 @@
 			{#if $modal.date}
 				<p class="text-xs">{$modal.date.toLocaleString()}</p>
 			{/if}
-			<Markdown content={$modal.description} class="cursor-text whitespace-pre-wrap pt-4 text-xs sm:text-sm" />
+			<Markdown content={$modal.description} class="cursor-text whitespace-pre-wrap pt-4 text-sm sm:text-md" />
 		</div>
 	{/if}
 </div>
