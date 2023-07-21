54d6114edc34e2e862d284f7de1c9b7fa73041e5
DeKok, Matt G <MGD3@na.valmont.com>
2023-07-21 10:05:06 -0500

    Remove comment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,7 +26,6 @@
 	<meta property="og:site_name" content="Adventurers League Log Sheet" />
 	<meta property="og:description" content={description.trim() || defaultDescription} />
 	<meta property="og:image" content={image?.trim() || defaultImage} />
-	<!-- <meta property="og:url" content={$page.url.href} /> -->
 	<meta property="og:type" content="website" />
 	<meta property="og:locale" content="en_US" />
 	<meta name="twitter:card" content="summary_large_image" />
