757df8f520b512c48e0cbf6767fc8ce422cdcc09
DeKok, Matt G <MGD3@na.valmont.com>
2023-10-11 11:11:52 -0500

    Date schema tweak

--- src/lib/types/schemas.ts
+++ src/lib/types/schemas.ts
@@ -3,8 +3,6 @@ import {
 	boolean,
 	custom,
 	date,
-	getOutput,
-	getPipeIssues,
 	literal,
 	maxLength,
 	merge,
@@ -23,18 +21,12 @@ import {
 	url,
 	type Input,
 	type Output,
-	type Pipe,
-	type PipeResult
+	type Pipe
 } from "valibot";
 
-export const dateSchema = transform(
-	nullable(union([literal(""), date(), string([iso()]), number([minValue(0)])], "Must be a valid date/time or unix timestamp")),
-	(input) => {
-		if (input === null || input === "") return null;
-		const d = new Date(input);
-		if (isNaN(d.getTime())) return null;
-		return d;
-	},
+export const nullableDateSchema = transform(
+	nullable(union([literal(""), date()])),
+	(input) => (!input ? null : new Date(input)),
 	[custom((input) => input === null || !isNaN(input.getTime()), "Invalid Date")]
 );
 
@@ -61,7 +53,7 @@ export type LogSchemaIn = Input<typeof logSchema>;
 export const logSchema = object({
 	id: nullish(string(), ""),
 	name: optional(string([minLength(1, "Log Name Required")]), ""),
-	date: nonNullable(dateSchema),
+	date: nonNullable(nullableDateSchema),
 	characterId: optional(string(), ""),
 	characterName: optional(string(), ""),
 	type: optional(union([literal("game"), literal("nongame")]), "game"),
@@ -74,7 +66,7 @@ export const logSchema = object({
 	description: nullish(string(), ""),
 	dm: dungeonMasterSchema,
 	is_dm_log: optional(boolean(), false),
-	applied_date: dateSchema,
+	applied_date: nullableDateSchema,
 	magic_items_gained: array(itemSchema("Item")),
 	magic_items_lost: array(string([minLength(1, "Invalid Item ID")])),
 	story_awards_gained: array(itemSchema("Story Award")),
@@ -95,57 +87,3 @@ export const newCharacterSchema = object({
 
 export type EditCharacterSchema = Output<typeof editCharacterSchema>;
 export const editCharacterSchema = merge([object({ id: string() }), newCharacterSchema]);
-
-/**
- * Custom Validators
- */
-
-// const dateRegex = /^((\d\d[2468][048]|\d\d[13579][26]|\d\d0[48]|[02468][048]00|[13579][26]00)-02-29|d{4}-((0[13578]|1[02])-(0[1-9]|[12]\d|3[01])|(0[469]|11)-(0[1-9]|[12]\d|30)|(02)-(0[1-9]|1\d|2[0-8])))T([01]\d|2[0-3]):[0-5]\d:[0-5]\d\.\d{3}([+-]([01]\d|2[0-3]):[0-5]\d|Z)$/;
-
-/**
- * Creates a complete, customizable validation function that validates a datetime.
- * The correct number of days in a month is validated, including leap year.
- *
- * Date Format: yyyy-mm-dd
- *
- * Time Formats: [T]hh:mm[:ss[.sss]][Z|+/-hh:mm]
- *
- * @param {Object} options The configuration options.
- * @param {boolean} options.date Whether to validate the date.
- * @param {boolean} options.time Whether to validate the time.
- * @param {boolean | "optional"} options.seconds Whether to validate the seconds.
- * @param {boolean | "optional"} options.milliseconds Whether to validate the milliseconds.
- * @param {boolean | "optional"} options.timezone Whether to validate the timezone.
- * @param {string} error The error message.
- *
- * @returns A validation function.
- */
-export function iso<TInput extends string>(options?: {
-	date?: boolean;
-	time?: boolean;
-	seconds?: boolean | "optional";
-	milliseconds?: boolean | "optional";
-	timezone?: boolean | "optional";
-	error?: string;
-}) {
-	return (input: TInput): PipeResult<TInput> => {
-		const {
-			date = true,
-			time = true,
-			seconds = "optional",
-			milliseconds = "optional",
-			timezone = "optional",
-			error = "Invalid ISO string"
-		} = options || {};
-
-		const dateRegex = `((\\d\\d[2468][048]|\\d\\d[13579][26]|\\d\\d0[48]|[02468][048]00|[13579][26]00)-02-29|\\d{4}-((0[13578]|1[02])-(0[1-9]|[12]\\d|3[01])|(0[469]|11)-(0[1-9]|[12]\\d|30)|(02)-(0[1-9]|1\\d|2[0-8])))`;
-		const millisecondsRegex = milliseconds ? `(\\.\\d{3})${milliseconds === "optional" ? "?" : ""}` : "";
-		const secondsRegex = seconds ? `(:[0-5]\\d${millisecondsRegex})${seconds === "optional" ? "?" : ""}` : "";
-		const timezoneRegex = timezone ? `([+-]([01]\\d|2[0-3]):[0-5]\\d|Z)${timezone === "optional" ? "?" : ""}` : "";
-		const timeRegex = `([01]\\d|2[0-3]):[0-5]\\d${secondsRegex}${timezoneRegex}`;
-		const regex = new RegExp(`^${date ? dateRegex : ""}${date && time ? "T" : time ? "T?" : ""}${time ? timeRegex : ""}$`);
-
-		if (!regex.test(input)) return getPipeIssues("iso", error, input);
-		return getOutput(input);
-	};
-}
