2c598dbce288deec722271333240399309f499c6
DeKok, Matt G <MGD3@na.valmont.com>
2023-11-15 15:17:29 -0600

    Add generic

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -6,7 +6,20 @@ export const pageLoader = writable(false);
 
 export const modal = writable<{ name: string; description: string; date?: Date } | null>(null);
 
-export const app = cookieStore("app", {
+export const app = cookieStore<{
+	settings: {
+		background: boolean;
+		theme: "system" | "dark" | "light";
+		mode: "dark" | "light";
+	};
+	character: {
+		descriptions: boolean;
+	};
+	characters: {
+		magicItems: boolean;
+		display: "list" | "grid";
+	};
+}>("app", {
 	settings: {
 		background: true,
 		theme: "system",
