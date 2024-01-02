d13c14e694f603f7245722eaadcacf1ba93caded
DeKok, Matt G <MGD3@na.valmont.com>
2024-01-02 16:11:04 -0600

    Update env schema

--- src/lib/types/schemas.ts
+++ src/lib/types/schemas.ts
@@ -18,12 +18,29 @@ import {
 	optional,
 	regex,
 	string,
+	undefined_,
 	union,
 	url,
+	value,
 	type Input,
 	type Output,
 	type Pipe
 } from "valibot";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const envSchema = (env: Record<string, any>) =>
+	object({
+		DATABASE_URL: string([url()]),
+		REDIS_URL: string([regex(/^rediss?:\/\//, "Must be a valid Redis URL")]),
+		AUTH_SECRET: string([minLength(10, "Must be a string of at least 10 characters")]),
+		AUTH_URL: string([url()]),
+		AUTH_TRUST_HOST: env["AUTH_URL"]?.includes("localhost")
+			? string([value("true", "Required. Must be 'true'")])
+			: undefined_("For localhost only"),
+		GOOGLE_CLIENT_ID: string([minLength(1, "Required")]),
+		GOOGLE_CLIENT_SECRET: string([minLength(1, "Required")]),
+		CRON_CHARACTER_ID: string([minLength(1, "Required")])
+	});
 
 export type DungeonMasterSchema = Output<typeof dungeonMasterSchema>;
 export const dungeonMasterSchema = object({
