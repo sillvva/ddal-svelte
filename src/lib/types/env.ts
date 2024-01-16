6b8bbf3eff4bd96c63a12b511101c9ec6ed9569b
DeKok, Matt G <MGD3@na.valmont.com>
2024-01-15 19:07:22 -0600

    Tweak

--- src/lib/types/env.ts
+++ src/lib/types/env.ts
@@ -4,12 +4,14 @@ import { envSchema } from "./schemas";
 export const checkEnv = async () => {
 	try {
 		const { default: def, ...env } = await import("$env/static/private");
+		if (!env) throw new Error("No environment variables found");
 
 		return parse(envSchema(env), {
 			...env,
-			AUTH_TRUST_HOST: env["AUTH_URL"]?.includes("localhost") ? env["AUTH_TRUST_HOST"] : undefined
+			AUTH_TRUST_HOST: env.AUTH_URL?.includes("localhost") ? env.AUTH_TRUST_HOST : undefined
 		});
 	} catch (err) {
+		if (err instanceof Error) console.error("❌ Invalid environment variables:\n", err.message);
 		if (err instanceof ValiError) {
 			console.error(
 				"❌ Invalid environment variables:\n",
