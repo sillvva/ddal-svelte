04e79001cf4b3981e80e076652b712fe7438626d
DeKok, Matt G <MGD3@na.valmont.com>
2024-02-03 10:49:06 -0600

    Don't await parent loader if unused

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -1,7 +1,6 @@
 import { redirect } from "@sveltejs/kit";
 
 export const load = async (event) => {
-	await event.parent();
 	const session = event.locals.session;
 
 	const redirectTo = event.url.searchParams.get("redirect");
