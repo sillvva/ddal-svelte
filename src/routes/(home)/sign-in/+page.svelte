1f9b917616d79946f540c0b0034f5ea141fc8b50
DeKok, Matt G <MGD3@na.valmont.com>
2024-02-08 18:21:19 -0600

    Auth and Vite updates

--- src/routes/(home)/sign-in/+page.svelte
+++ src/routes/(home)/sign-in/+page.svelte
@@ -1,21 +1,3 @@
-<script lang="ts">
-	import { goto } from "$app/navigation";
-	import { page } from "$app/stores";
-	import { signIn } from "@auth/sveltekit/client";
-	import { onMount } from "svelte";
-
-	onMount(() => {
-		setTimeout(() => {
-			const redirectTo = $page.url.searchParams.get("redirect") || "/characters";
-			if (!$page.data.session?.user) {
-				signIn("google", { callbackUrl: `${$page.url.origin}${redirectTo}` });
-			} else {
-				goto(redirectTo, { replaceState: true });
-			}
-		}, 500);
-	});
-</script>
-
 <main class="container relative mx-auto flex min-h-dvh flex-col items-center justify-center p-4">
 	<h1>Signing in...</h1>
 </main>
