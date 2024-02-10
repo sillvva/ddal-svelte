d9c3dbd76c5257924b64cf4259fef823a0f07da5
DeKok, Matt G <MGD3@na.valmont.com>
2024-02-09 18:04:00 -0600

    Remove types from JSDoc

--- src/server/cache.ts
+++ src/server/cache.ts
@@ -11,10 +11,10 @@ export type CacheKey = [string, ...string[]];
 /**
  * Retrieves a cache from Redis or caches the results of a callback function.
  * @template TReturnType The return type of the callback function.
- * @param {() => Promise<TReturnType>} callback The callback function to cache.
- * @param {CacheKey} key The cache key as an array of strings.
- * @param {number} [expires=259200] The cache expiration time in seconds. Defaults to 3 days.
- * @returns {Promise<TReturnType>} The cached result of the callback function.
+ * @param [callback] The callback function to cache.
+ * @param [key] The cache key as an array of strings.
+ * @param [expires=259200] The cache expiration time in seconds. Defaults to 3 days.
+ * @returns The cached result of the callback function.
  */
 export async function cache<TReturnType>(callback: () => Promise<TReturnType>, key: CacheKey, expires = 3 * 86400) {
 	const rkey = key.join("|");
@@ -40,11 +40,10 @@ export async function cache<TReturnType>(callback: () => Promise<TReturnType>, k
 /**
  * Retrieves caches from Redis or caches the results of a callback function for each key.
  * @template TReturnType The return type of the callback function.
- * @param {(keys: CacheKey[], hits: TReturnType[]) => Promise<Array<{ key: CacheKey; value: TReturnType }>} callback
- * The callback function to cache. The hits parameter is an array of cached results of the callback function for each key.
- * @param {CacheKey[]} [keys] The cache keys as an array of arrays of strings.
- * @param {number} [expires=259200] The cache expiration time in seconds. Defaults to 3 days.
- * @returns {Promise<TReturnType[]>} An array of cached results of the callback function for each key.
+ * @param [callback] The callback function to cache. The hits parameter is an array of cached results of the callback function for each key.
+ * @param [keys] The cache keys as an array of arrays of strings.
+ * @param [expires=259200] The cache expiration time in seconds. Defaults to 3 days.
+ * @returns An array of cached results of the callback function for each key.
  */
 export async function mcache<TReturnType>(
 	callback: (keys: CacheKey[], hits: TReturnType[]) => Promise<Array<{ key: CacheKey; value: TReturnType }>>,
@@ -105,8 +104,7 @@ export async function mcache<TReturnType>(
 
 /**
  * Invalidates Redis caches based on an array of keys.
- * @param {Array<CacheKey | "" | false | null | undefined>} keys The cache keys as an array of arrays of strings. Empty strings, false, null, and undefined are ignored.
- * @returns {void}
+ * @param [keys] The cache keys as an array of arrays of strings. Empty strings, false, null, and undefined are ignored.
  */
 export function revalidateKeys(keys: Array<CacheKey | "" | false | null | undefined>) {
 	const cacheKeys = keys.filter((t) => Array.isArray(t) && t.length).map((t) => (t as string[]).join("|"));
