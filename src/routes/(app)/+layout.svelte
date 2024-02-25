80eaf0b04a755ed0ba481fa4893d82a235922b2d
DeKok, Matt G <MGD3@na.valmont.com>
2024-02-25 11:19:51 -0600

    Refactor Command.List component in +layout.svelte

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -372,91 +372,95 @@
 				<Command.List class="flex max-h-96 flex-col gap-2 overflow-y-scroll" bind:el={resultsPane}>
 					<Command.Empty class="p-4 text-center font-bold">No results found.</Command.Empty>
 
-					{#each results as section, i}
-						{#if i > 0}
-							<div class="divider" />
-						{/if}
-						<Command.Group asChild let:group>
-							<ul class="menu p-0" {...group.attrs}>
-								<li class="menu-title">{section.title}</li>
-								{#each section.items as item}
-									<Command.Item asChild let:attrs value={item.url}>
-										<li
-											{...attrs}
-											data-selected={selected === item.url ? "true" : undefined}
-											class:selected={selected === item.url}
-										>
-											<a href={item.url} on:click={() => (cmdOpen = false)} class="flex gap-4 [.selected>&]:bg-neutral-500/40">
-												{#if item.type === "character"}
-													<span class="mask mask-squircle h-12 min-w-12 max-w-12 bg-primary">
-														<img src={item.image_url} class="size-full object-cover object-top transition-all" alt={item.name} />
-													</span>
-													<div class="flex flex-col">
-														<div>{item.name}</div>
-														<div class="text-xs opacity-70">
-															Level {item.total_level}
-															{item.race}
-															{item.class}
-														</div>
-														{#if search.length >= 2}
-															{#if item.magic_items.some((magicItem) => hasMatch(magicItem.name))}
-																<div class="flex gap-1 text-xs">
-																	<span class="whitespace-nowrap font-bold">Magic Items:</span>
-																	<span class="flex-1 opacity-70">
-																		{[...new Set(item.magic_items.map((item) => item.name))]
-																			.filter((item) => hasMatch(item))
-																			.sort((a, b) => sorter(a, b))
-																			.join(", ")}
-																	</span>
-																</div>
-															{/if}
-															{#if item.story_awards.some((storyAward) => hasMatch(storyAward.name))}
-																<div class="flex gap-2 text-xs">
-																	<span class="whitespace-nowrap font-bold">Story Awards:</span>
-																	<span class="flex-1 opacity-70">
-																		{[...new Set(item.story_awards.map((item) => item.name))]
-																			.filter((item) => hasMatch(item))
-																			.sort((a, b) => sorter(a, b))
-																			.join(", ")}
-																	</span>
-																</div>
-															{/if}
-														{/if}
-													</div>
-												{:else if item.type === "log"}
-													<div class="flex flex-col">
-														<div>{item.name}</div>
-														<div class="flex gap-2 opacity-70">
-															<span class="text-xs">{new Date(item.date).toLocaleDateString()}</span>
-															<div class="divider divider-horizontal mx-0 w-0" />
-															{#if item.character}
-																<span class="text-xs">{item.character.name}</span>
-															{:else}
-																<span class="text-xs italic">Unassigned</span>
-															{/if}
-														</div>
-														{#if search.length >= 2}
-															{#if item.dm && hasMatch(item.dm.name)}
-																<div class="flex gap-1 text-xs">
-																	<span class="whitespace-nowrap font-bold">DM:</span>
-																	<span class="flex-1 opacity-70">{item.dm.name}</span>
-																</div>
-															{/if}
-														{/if}
-													</div>
-												{:else}
-													{item.name}
-												{/if}
-											</a>
-										</li>
-									</Command.Item>
-								{/each}
-							</ul>
-						</Command.Group>
-					{/each}
-
 					{#if !searchData.length}
 						<div class="p-4 text-center font-bold">Loading data...</div>
+					{:else}
+						{#each results as section, i}
+							{#if i > 0}
+								<div class="divider" />
+							{/if}
+							<Command.Group asChild let:group>
+								<ul class="menu p-0" {...group.attrs}>
+									<li class="menu-title">{section.title}</li>
+									{#each section.items as item}
+										<Command.Item asChild let:attrs value={item.url}>
+											<li
+												{...attrs}
+												data-selected={selected === item.url ? "true" : undefined}
+												class:selected={selected === item.url}
+											>
+												<a href={item.url} on:click={() => (cmdOpen = false)} class="flex gap-4 [.selected>&]:bg-neutral-500/40">
+													{#if item.type === "character"}
+														<span class="mask mask-squircle h-12 min-w-12 max-w-12 bg-primary">
+															<img
+																src={item.image_url}
+																class="size-full object-cover object-top transition-all"
+																alt={item.name}
+															/>
+														</span>
+														<div class="flex flex-col">
+															<div>{item.name}</div>
+															<div class="text-xs opacity-70">
+																Level {item.total_level}
+																{item.race}
+																{item.class}
+															</div>
+															{#if search.length >= 2}
+																{#if item.magic_items.some((magicItem) => hasMatch(magicItem.name))}
+																	<div class="flex gap-1 text-xs">
+																		<span class="whitespace-nowrap font-bold">Magic Items:</span>
+																		<span class="flex-1 opacity-70">
+																			{[...new Set(item.magic_items.map((item) => item.name))]
+																				.filter((item) => hasMatch(item))
+																				.sort((a, b) => sorter(a, b))
+																				.join(", ")}
+																		</span>
+																	</div>
+																{/if}
+																{#if item.story_awards.some((storyAward) => hasMatch(storyAward.name))}
+																	<div class="flex gap-2 text-xs">
+																		<span class="whitespace-nowrap font-bold">Story Awards:</span>
+																		<span class="flex-1 opacity-70">
+																			{[...new Set(item.story_awards.map((item) => item.name))]
+																				.filter((item) => hasMatch(item))
+																				.sort((a, b) => sorter(a, b))
+																				.join(", ")}
+																		</span>
+																	</div>
+																{/if}
+															{/if}
+														</div>
+													{:else if item.type === "log"}
+														<div class="flex flex-col">
+															<div>{item.name}</div>
+															<div class="flex gap-2 opacity-70">
+																<span class="text-xs">{new Date(item.date).toLocaleDateString()}</span>
+																<div class="divider divider-horizontal mx-0 w-0" />
+																{#if item.character}
+																	<span class="text-xs">{item.character.name}</span>
+																{:else}
+																	<span class="text-xs italic">Unassigned</span>
+																{/if}
+															</div>
+															{#if search.length >= 2}
+																{#if item.dm && hasMatch(item.dm.name)}
+																	<div class="flex gap-1 text-xs">
+																		<span class="whitespace-nowrap font-bold">DM:</span>
+																		<span class="flex-1 opacity-70">{item.dm.name}</span>
+																	</div>
+																{/if}
+															{/if}
+														</div>
+													{:else}
+														{item.name}
+													{/if}
+												</a>
+											</li>
+										</Command.Item>
+									{/each}
+								</ul>
+							</Command.Group>
+						{/each}
 					{/if}
 				</Command.List>
 			</Command.Dialog>
