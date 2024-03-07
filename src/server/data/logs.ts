4001f9d2b879fd2980d7fc8affc5d0153f1c4b08
DeKok, Matt G <MGD3@na.valmont.com>
2024-03-06 21:39:15 -0600

    Refactor log parsing and add missing null check

--- src/server/data/logs.ts
+++ src/server/data/logs.ts
@@ -1,10 +1,10 @@
-import { defaultDM, defaultLogData } from "$lib/entities";
+import { defaultDM, defaultLogData, parseLog } from "$lib/entities";
 import { prisma } from "$src/server/db";
 import type { DungeonMaster, Log, MagicItem, StoryAward } from "@prisma/client";
 import { cache } from "../cache";
 
 export type LogData = Log & {
-	dm: DungeonMaster;
+	dm: DungeonMaster | null;
 	magic_items_gained: MagicItem[];
 	magic_items_lost: MagicItem[];
 	story_awards_gained: StoryAward[];
@@ -22,7 +22,7 @@ export async function getLog(logId: string, userId: string, characterId = ""): P
 				story_awards_lost: true
 			}
 		})) || defaultLogData(userId, characterId);
-	return { ...log, dm: log.dm || defaultDM(userId) };
+	return { ...(parseLog(log) || ""), dm: log.dm || defaultDM(userId) };
 }
 
 export async function getDMLog(logId: string, userId: string): Promise<LogData> {
@@ -37,46 +37,53 @@ export async function getDMLog(logId: string, userId: string): Promise<LogData> 
 				story_awards_lost: true
 			}
 		})) || defaultLogData(userId);
-	return { ...log, dm: log.dm || defaultDM(userId) };
+	return { ...(parseLog(log) || ""), dm: log.dm || defaultDM(userId) };
 }
 
 export type DMLogsData = Awaited<ReturnType<typeof getDMLogs>>;
 export async function getDMLogs(userId = "", userName = "") {
-	return prisma.log.findMany({
-		where: {
-			is_dm_log: true,
-			dm: {
-				OR: [
-					{
-						uid: userId
+	return prisma.log
+		.findMany({
+			where: {
+				is_dm_log: true,
+				dm: {
+					OR: [
+						{
+							uid: userId
+						},
+						{
+							name: userName
+						}
+					]
+				}
+			},
+			include: {
+				dm: true,
+				magic_items_gained: true,
+				magic_items_lost: true,
+				story_awards_gained: true,
+				story_awards_lost: true,
+				character: {
+					include: {
+						user: true
 					},
-					{
-						name: userName
-					}
-				]
-			}
-		},
-		include: {
-			dm: true,
-			magic_items_gained: true,
-			magic_items_lost: true,
-			story_awards_gained: true,
-			story_awards_lost: true,
-			character: {
-				include: {
-					user: true
-				},
-				where: {
-					id: {
-						not: ""
+					where: {
+						id: {
+							not: ""
+						}
 					}
 				}
+			},
+			orderBy: {
+				date: "asc"
 			}
-		},
-		orderBy: {
-			date: "asc"
-		}
-	});
+		})
+		.then((logs) => {
+			return logs.map((log) => ({
+				...log,
+				...parseLog(log)
+			}));
+		});
 }
 
 export async function getDMLogsCache(userId = "", userName = "") {
