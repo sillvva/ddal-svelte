2191004654757ce5edb34bd1708c892c972e5835
DeKok, Matt G <MGD3@na.valmont.com>
2024-03-07 13:04:20 -0600

    Update Svelte adapter runtime to "edge"

--- svelte.config.js
+++ svelte.config.js
@@ -8,13 +8,14 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), docPreprocess()],
-	runtime: "edge",
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: "edge"
+		}),
 		alias: {
 			$lib: path.resolve("./src/lib"),
 			$src: path.resolve("./src")
