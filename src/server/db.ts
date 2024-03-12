dad0dba4bcde60f7f71b1a349682324172003dd5
DeKok, Matt G <MGD3@na.valmont.com>
2024-03-12 14:03:22 -0500

    Update postgres connection options

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,7 +3,7 @@ import * as schema from "$src/db/schema";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const client = postgres(privateEnv.DATABASE_URL);
+const client = postgres(privateEnv.DATABASE_URL, { prepare: false });
 
 export const db = drizzle(client, { schema });
 export const q = db.query;
