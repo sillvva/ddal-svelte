e52ee38d309da4c15fec9ea8e86b4a180198fe1b
DeKok, Matt G <MGD3@na.valmont.com>
2024-03-14 17:52:22 -0500

    Update VSCode settings and database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	schema: "./src/db/schema.ts",
+	schema: "./src/server/db/schema.ts",
 	out: "./drizzle",
 	driver: "pg",
 	dbCredentials: {
