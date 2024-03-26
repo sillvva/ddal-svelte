7ae43aba2c33ddb726735b36e921f8415660ce1a
DeKok, Matt G <MGD3@na.valmont.com>
2024-03-26 16:58:36 -0500

    Update darkMode in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ import plugin from "tailwindcss/plugin";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
-	darkMode: ["selector"],
+	darkMode: ["class"],
 	theme: {
 		extend: {
 			fontFamily: {
