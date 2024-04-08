54a70f766c9dd386e2ec7df46cd81b29455a0de3
DeKok, Matt G <MGD3@na.valmont.com>
2024-04-08 14:39:27 -0500

    Fix styling and remove unnecessary class in BackButton.svelte

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -2,7 +2,7 @@
 	export let href = "";
 </script>
 
-<a {href} class="back-btn mb-4 flex h-12 flex-1 items-center gap-4 text-secondary sm:hidden">
+<a {href} class="mb-4 flex h-12 flex-1 items-center gap-4 text-secondary sm:hidden">
 	<span class="iconify size-6 mdi-chevron-left" />
 	<span>Back to <slot /></span>
 </a>
