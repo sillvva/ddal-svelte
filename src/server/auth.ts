b1bd895fa87b1595814bb1c9f46e787e2129537a
DeKok, Matt G <MGD3@na.valmont.com>
2024-04-10 16:49:55 -0500

    Fix redirect URL in signInRedirect function

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,13 +4,12 @@ import type { User } from "@auth/sveltekit";
 import { redirect } from "@sveltejs/kit";
 
 /**
- * Redirects to /sign-in with a redirect query parameter
+ * Redirects to / with a redirect query parameter
  * @param url - The URL to redirect to after signing in
  * @throws {Redirect} Redirects to /sign-in
  * @return {never}
  */
 export function signInRedirect(url: URL): never {
-	// redirect(302, `/sign-in?redirect=${encodeURIComponent(`${url.pathname}${url.search}`)}`);
 	redirect(302, `/?redirect=${encodeURIComponent(`${url.pathname}${url.search}`)}`);
 }
 
