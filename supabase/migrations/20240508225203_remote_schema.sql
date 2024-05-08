7cdd6fd8453199e5c9e969897c3994e0d59a0d9d
DeKok, Matt G <MGD3@na.valmont.com>
2024-05-08 17:56:06 -0500

    Add supabase migration

---
+++ supabase/migrations/20240508225203_remote_schema.sql
@@ -0,0 +1,13 @@
+alter table "public"."account" drop constraint "account_userId_fkey";
+
+alter table "public"."session" drop constraint "session_userId_fkey";
+
+alter table "public"."account" add constraint "public_account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."account" validate constraint "public_account_userId_fkey";
+
+alter table "public"."session" add constraint "public_session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."session" validate constraint "public_session_userId_fkey";
+
+
