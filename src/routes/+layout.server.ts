992948340446208a151069a333f7fa346c516615
DeKok, Matt G <MGD3@na.valmont.com>
2024-06-24 06:03:49 -0500

    Debugging coolify predefined variables

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,19 @@
 import { building } from "$app/environment";
+import * as dyn from "$env/static/private";
+import * as env from "$env/static/private";
 import { privateEnv } from "$lib/env/private.js";
 import { serverGetCookie } from "$server/cookie.js";
+
+const filteredObj = (obj: Record<PropertyKey, unknown> | NodeJS.ProcessEnv) => {
+	return Object.entries(obj)
+  .filter(([key]) => key.startsWith("COOLIFY_"))
+  .reduce((obj, [key, val]) => {
+    return {
+      ...obj,
+      [key]: val
+    };
+  }, {});
+};
 
 let checked = false;
 if (!checked && building && privateEnv) {
@@ -8,6 +21,26 @@ if (!checked && building && privateEnv) {
 	console.table(privateEnv);
 	console.log("\n");
 	checked = true;
+
+	console.log("\nCoolify check: process.env\n");
+	const filteredPE = filteredObj(process.env);
+	console.table(Object.keys(filteredPE).length ? filteredPE : { "(none)": "(none)" });
+	console.log("\n");
+
+	console.log("\nCoolify check: import.meta.env\n");
+	const filteredME = filteredObj(import.meta.env);
+	console.table(Object.keys(filteredME).length ? filteredME : { "(none)": "(none)" });
+	console.log("\n");
+
+	console.log("\nCoolify check: env\n");
+	const filteredE = filteredObj(env);
+	console.table(Object.keys(filteredE).length ? filteredE : { "(none)": "(none)" });
+	console.log("\n");
+
+	console.log("\nCoolify check: dyn\n");
+	const filteredD = filteredObj(dyn);
+	console.table(Object.keys(filteredD).length ? filteredD : { "(none)": "(none)" });
+	console.log("\n");
 }
 
 export const load = async (event) => {
