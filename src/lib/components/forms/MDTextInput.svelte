3a75890e1fa861204151caffe63da4adbffdd92a
DeKok, Matt G <matt.dekok@gmail.com>
2024-10-12 23:40:40 -0500

    Use grapheme count instead of string length

--- src/lib/components/forms/MDTextInput.svelte
+++ src/lib/components/forms/MDTextInput.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+	import autosize from "svelte-autosize";
 	import { formFieldProxy, type FormPathLeaves, type SuperForm } from "sveltekit-superforms";
 	import { twMerge } from "tailwind-merge";
 	import Markdown from "../Markdown.svelte";
-	import AutoResizeTextArea from "./AutoResizeTextArea.svelte";
 
 	type T = $$Generic<Record<PropertyKey, unknown>>;
 
@@ -16,6 +16,9 @@
 	let state = "edit";
 
 	const { value, errors, constraints } = formFieldProxy(superform, field);
+
+	$: graphemeCount = typeof $value === "string" ? [...new Intl.Segmenter().segment($value)].length : 0;
+	$: lengthDiff = $value.length - graphemeCount;
 </script>
 
 <label for={field} class="label">
@@ -27,17 +30,21 @@
 		<input type="radio" {name} role="tab" class="tab !rounded-md" aria-label="Preview" bind:group={state} value="preview" />
 	</div>
 {/if}
-<AutoResizeTextArea
+<textarea
+	{...$$restProps}
 	id={field}
-	bind:content={$value}
+	bind:value={$value}
 	class={twMerge(
 		"textarea textarea-bordered w-full focus:border-primary",
 		preview && "rounded-t-none",
 		state === "preview" && "hidden"
 	)}
-	{minRows}
-	{maxRows}
+	style:--minRows={minRows && `${minRows}lh`}
+	style:--maxRows={maxRows && `${maxRows}lh`}
+	spellcheck="true"
+	use:autosize
 	{...$constraints}
+	maxlength={($constraints?.maxlength ?? 0) + lengthDiff}
 />
 {#if preview && state === "preview"}
 	<div class="rounded-b-lg border-[1px] border-base-content bg-base-100 p-4 [--tw-border-opacity:0.2]">
@@ -51,7 +58,15 @@
 			<span class="label-text-alt">Markdown Allowed</span>
 		{/if}
 		{#if !$errors?.length && $constraints?.maxlength}
-			<span class="label-text-alt">{`${$value}`.length.toLocaleString()} / {$constraints?.maxlength.toLocaleString()}</span>
+			<span class="label-text-alt">{graphemeCount.toLocaleString()} / {$constraints?.maxlength.toLocaleString()}</span>
 		{/if}
 	</label>
 {/if}
+
+<style>
+	textarea {
+		resize: none;
+		min-height: calc(var(--minRows, 3lh) + 1rem + 2px);
+		max-height: calc(var(--maxRows, 50lh) + 1rem + 2px);
+	}
+</style>
