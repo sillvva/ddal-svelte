decfb164d937cbe3b5d35f70dd6c1b1dc6874746
DeKok, Matt G <matt.dekok@gmail.com>
2024-12-20 09:47:49 -0600

    Update dependencies and refactor schemas for improved nullability handling
    - Updated various dependencies in package.json and pnpm-lock.yaml to their latest versions, including Svelte, TypeScript, and ESLint-related packages.
    - Refactored schema definitions in schemas.ts to use nullable types for several properties, enhancing data validation and default value handling.
    - Improved meta tag handling in layout.svelte by removing default fallbacks and ensuring consistent title and description usage.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -32,27 +32,27 @@
 </script>
 
 <svelte:head>
-	<title>{title.trim() || defaultTitle}</title>
-	<meta name="title" content={title.trim() || defaultTitle} />
-	<meta name="description" content={description.trim() || defaultDescription} />
-	<meta property="og:title" content={title.trim() || defaultTitle} />
+	<title>{title.trim()}</title>
+	<meta name="title" content={title.trim()} />
+	<meta name="description" content={description.trim()} />
+	<meta property="og:title" content={title.trim()} />
 	<meta property="og:site_name" content="Adventurers League Log Sheet" />
-	<meta property="og:description" content={description.trim() || defaultDescription} />
-	<meta property="og:image" content={image?.trim() || defaultImage} />
+	<meta property="og:description" content={description.trim()} />
+	<meta property="og:image" content={image.trim()} />
 	<meta property="og:type" content="website" />
 	<meta property="og:locale" content="en_US" />
 	<meta property="og:url" content={page.url.toString()} />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:creator" content="@sillvvasensei" />
 	<meta name="twitter:creator:id" content="1006748654391169029" />
-	<meta name="twitter:title" content={title.trim() || defaultTitle} />
+	<meta name="twitter:title" content={title.trim()} />
 	<meta name="twitter:site" content="Adventurers League Log Sheet" />
-	<meta name="twitter:description" content={description.trim() || defaultDescription} />
-	<meta name="twitter:image" content={image?.trim() || defaultImage} />
+	<meta name="twitter:description" content={description.trim()} />
+	<meta name="twitter:image" content={image.trim()} />
 	<link rel="canonical" href={page.url.toString()} />
 </svelte:head>
 
-{#if global.pageLoader || navigating.current}
+{#if global.pageLoader || navigating.type}
 	<div
 		class="fixed inset-0 z-40 flex items-center justify-center bg-black/50"
 		in:fade={{ duration: 100, delay: 400 }}
