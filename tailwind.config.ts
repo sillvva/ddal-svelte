f5ae983f19a4039b14c6aaf6aab5f9ded5388cc5
DeKok, Matt G <matt.dekok@gmail.com>
2025-01-25 11:30:32 -0600

    Remove content configuration from Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,5 @@ import { addIconSelectors } from "@iconify/tailwind";
 import type { Config } from "tailwindcss";
 
 export default {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
 	plugins: [addIconSelectors(["mdi", "material-symbols", "logos"])]
 } satisfies Config;
