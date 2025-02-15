a8427f1a28c17cfd308c5812ea27f884d84e24bb
DeKok, Matt G. <matt.dekok@gmail.com>
2025-02-14 23:41:54 -0600

    Add HTML renderers

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -3,6 +3,13 @@
 	// Renderers
 	import blockquote from "$lib/components/renderers/blockquote.svelte";
 	import heading from "$lib/components/renderers/heading.svelte";
+	import h1 from "$lib/components/renderers/html/h1.svelte";
+	import h2 from "$lib/components/renderers/html/h2.svelte";
+	import h3 from "$lib/components/renderers/html/h3.svelte";
+	import h4 from "$lib/components/renderers/html/h4.svelte";
+	import img from "$lib/components/renderers/html/img.svelte";
+	import ol from "$lib/components/renderers/html/ol.svelte";
+	import ul from "$lib/components/renderers/html/ul.svelte";
 	import image from "$lib/components/renderers/image.svelte";
 	import link from "$lib/components/renderers/link.svelte";
 	import list from "$lib/components/renderers/list.svelte";
@@ -37,7 +44,30 @@
 			tablehead,
 			tablebody,
 			tablerow,
-			tablecell
+			tablecell,
+			html: {
+				iframe: null,
+				audio: null,
+				video: null,
+				embed: null,
+				canvas: null,
+				source: null,
+				track: null,
+				ol,
+				ul,
+				table,
+				thead: tablehead,
+				tbody: tablebody,
+				tr: tablerow,
+				td: tablecell,
+				p: paragraph,
+				a: link,
+				h1,
+				h2,
+				h3,
+				h4,
+				img
+			}
 		}}
 	/>
 </div>
---
+++ src/lib/components/renderers/html/h2.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import { twMerge } from "tailwind-merge";
+
+	interface Props {
+		children?: Snippet;
+		attributes?: Record<string, any>;
+	}
+
+	const { children, attributes }: Props = $props();
+</script>
+
+<h2 {...attributes} class={twMerge("mt-6 mb-2 text-2xl font-bold first:mt-0", attributes?.class)}>{@render children?.()}</h2>
