ae550804e3f25bc5faa7bd770361a59eabba285b
DeKok, Matt G. <matt.dekok@gmail.com>
2025-03-19 08:47:06 -0500

    Enhance MDTextInput component with grapheme character handling and tooltips
    - Introduce graphemeCount and lengthDiff calculations for better character visibility management
    - Add a tooltip to display grapheme character usage and special character effects
    - Update the display logic to conditionally show maximum length and grapheme information

--- src/lib/components/forms/MDTextInput.svelte
+++ src/lib/components/forms/MDTextInput.svelte
@@ -30,9 +30,13 @@
 
 	const graphemeCount = $derived(typeof $value === "string" ? [...new Intl.Segmenter().segment($value)].length : 0);
 	const lengthDiff = $derived($value.length - graphemeCount);
-	const maxLength = $derived($constraints?.maxlength ? $constraints?.maxlength + lengthDiff : undefined);
 
 	const markdownTip = "Both Markdown and HTML are allowed. However, some elements are not allowed.";
+	const graphemeTip = $derived(
+		lengthDiff
+			? `You have used ${graphemeCount} characters, but ${lengthDiff} are not visible. This is caused by the use of special characters like emojis.`
+			: undefined
+	);
 </script>
 
 <label for={field} class="fieldset-legend">
@@ -59,7 +63,6 @@
 		spellcheck="true"
 		use:autosize
 		{...$constraints}
-		maxlength={maxLength}
 	></textarea>
 	{#if preview && state === "preview"}
 		<div class="border-base-content/20 bg-base-100 rounded-b-lg border-[1px] p-4 [--tw-border-opacity:0.2]">
@@ -72,10 +75,18 @@
 		{#if $errors?.length}
 			<span class="text-error">{$errors}</span>
 		{:else}
-			<span class="tooltip tooltip-bottom" data-tip={markdownTip}>Markdown Allowed</span>
+			<span class="tooltip tooltip-bottom" data-tip={markdownTip}>
+				Markdown Allowed
+				<span class="iconfify mdi--question-mark-circle"></span>
+			</span>
 		{/if}
-		{#if !$errors?.length && maxLength}
-			<span>{graphemeCount.toLocaleString()} / {maxLength.toLocaleString()}</span>
+		{#if !$errors?.length && $constraints?.maxlength}
+			<span class="tooltip tooltip-bottom" data-tip={graphemeTip}>
+				{$value.length.toLocaleString()} / {$constraints?.maxlength.toLocaleString()}
+				{#if graphemeTip}
+					<span class="iconfify mdi--question-mark-circle"></span>
+				{/if}
+			</span>
 		{/if}
 	</label>
 {/if}
