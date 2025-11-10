88e54c8dd3bb6ded898924d23e61f8972d21c880
Matt DeKok <matt@dekok.dev>
2025-11-09 23:14:40 -0600

    refactor: implement user ban and unban functionality with server-side forms

--- src/lib/remote/admin/forms.remote.ts
+++ src/lib/remote/admin/forms.remote.ts
@@ -1,4 +1,4 @@
-import { userIdSchema } from "$lib/schemas";
+import { requiredString, userIdSchema } from "$lib/schemas";
 import { guardedForm } from "$lib/server/effect/remote";
 import { AdminService } from "$lib/server/effect/services/admin";
 import { AuthService } from "$lib/server/effect/services/auth";
@@ -37,3 +37,31 @@ export const stopImpersonating = guardedForm(function* (_, { event }) {
 
 	redirect(302, "/admin/users");
 });
+
+export const banUser = guardedForm(
+	v.object({ userId: userIdSchema, banReason: requiredString }),
+	function* ({ userId, banReason }, { event, invalid }) {
+		const Auth = yield* AuthService;
+		const Admin = yield* AdminService;
+
+		const auth = yield* Auth.auth();
+		const user = yield* Admin.get.user(userId).pipe(Effect.catchAll((err) => Effect.die(invalid(err.message))));
+
+		if (user.role === "admin") throw invalid("Cannot ban admins");
+		if (user.banned) throw invalid("User is already banned");
+
+		return yield* Effect.promise(() => auth.api.banUser({ body: { userId, banReason }, headers: event.request.headers }));
+	},
+	true
+);
+
+export const unbanUser = guardedForm(
+	v.object({ userId: userIdSchema }),
+	function* ({ userId }, { event }) {
+		const Auth = yield* AuthService;
+
+		const auth = yield* Auth.auth();
+		return yield* Effect.promise(() => auth.api.unbanUser({ body: { userId }, headers: event.request.headers }));
+	},
+	true
+);
